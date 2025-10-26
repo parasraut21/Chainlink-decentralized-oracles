2cf76bd031881f9ddb5d0682d67fe97844ad570d
parasraut21 <parasraut2511@gmail.com>
2025-10-26 16:51:31 +0530

    feat: add utility functions for common contract operations

---
+++ utils/helpers.js
@@ -0,0 +1,37 @@
+/**
+ * Utility functions for Chainlink contract interactions
+ */
+
+/**
+ * Check if the current network is a local development network
+ * @param {string} networkName - The name of the current network
+ * @returns {boolean} True if it's a local network
+ */
+function isLocalNetwork(networkName) {
+    const localNetworks = ["hardhat", "localhost", "ganache"];
+    return localNetworks.includes(networkName);
+}
+
+/**
+ * Format BigInt values for display
+ * @param {BigInt} value - The BigInt value to format
+ * @returns {string} Formatted string representation
+ */
+function formatBigInt(value) {
+    return BigInt(value).toString();
+}
+
+/**
+ * Validate Ethereum address format
+ * @param {string} address - The address to validate
+ * @returns {boolean} True if valid address format
+ */
+function isValidAddress(address) {
+    return address && ethers.utils.isAddress(address);
+}
+
+module.exports = {
+    isLocalNetwork,
+    formatBigInt,
+    isValidAddress
+};
